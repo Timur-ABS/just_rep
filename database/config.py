ac6504bd74b2960fdf93de7a752c105696015af2
Timur-ABS <83013236+Timur-ABS@users.noreply.github.com>
2023-04-21 23:59:12 +0300

    Update config.py

--- database/config.py
+++ database/config.py
@@ -2,6 +2,6 @@ db_config = {
     'host': '5.101.44.221',
     'user': 'timurabs',
     'port': 3306,
-    'password': 'zgABukMTayBtdycLO7V7RQPSpLHjLjBT5AFKXwXulYSVho9IJ4a8NAdmAbbJL7jE!',
-    'db': 'GPbet_database'
+    'password': 'your_password',
+    'db': 'table_name'
 }
